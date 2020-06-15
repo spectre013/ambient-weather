d9d92a1987d756bfa99f6d430abda09b0f048cdb
Brian Paulson <brian.paulson@polarisalpha.com>
2020-06-15 13:31:03 -0600

    Security and upgrade fixes

--- weather-ui/build.sh
+++ weather-ui/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build . --no-cache -t docker.zoms.net/weather-ui:1.0
+docker build . --no-cache -t docker.zoms.net/weather-ui:1.1
 if [[ $1 == 'push' ]]
 then
-    docker push docker.zoms.net/weather-ui:1.0
+    docker push docker.zoms.net/weather-ui:1.1
 fi
