509d62c071f1c85ae280482d11918a9014fa113e
Brian Paulson <brian.paulson@polarisalpha.com>
2021-02-23 19:13:35 -0700

    Backup Updates

---
+++ weather-server/alerts.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var LastModified time.Time
+
+func parseJson(j []byte) {
+	alerts := Alerts{}
+	err := json.Unmarshal(j, &alerts)
+	if err != nil {
+		logger.Error(err)
+	}
+	for k, v := range alerts.Features {
+		fmt.Println(k, v.Properties.Event)
+	}
+}
+
+func startAlerts() {
+	logger.Info("Running Alert System")
+	alertCheck()
+	for range time.NewTicker(1 * time.Minute).C {
+		alertCheck()
+	}
+}
+
+func alertCheck() bool {
+	uri := "https://api.weather.gov/alerts/active?area=CO"
+
+	if !LastModified.IsZero() {
+		_, err := alertRequest("HEAD", uri)
+		if err != nil {
+			logger.Error(err)
+			return false
+		}
+	}
+
+	res, err := alertRequest("GET", uri)
+	if err != nil {
+		logger.Error()
+		return false
+	}
+	parseJson(res)
+	return true
+}
+
+func alertRequest(t string, url string) (body []byte, err error) {
+	body = []byte("")
+	client := &http.Client{}
+	req, err := http.NewRequest(t, url, nil)
+	req.Header.Add("User-Agent", `Zoms Weather, wxcos@zoms.net`)
+
+	resp, err := client.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		err = errors.New("server responded with an error")
+		return body, err
+	}
+	if _, ok := resp.Header["Last-Modified"]; ok {
+		lModified := ""
+		if len(resp.Header["Last-Modified"]) > 0 {
+			lModified = resp.Header["Last-Modified"][0]
+		}
+		l, err := time.Parse("Mon, 2 Jan 2006 15:04:05 GMT", lModified)
+		if err != nil {
+			return []byte(""), err
+		}
+		logger.Info(l.Before(LastModified), l, LastModified)
+		if (l.Before(LastModified) || l.Equal(LastModified)) && !LastModified.IsZero() {
+			logger.Debug("No Updates")
+			logger.Debug(l, LastModified)
+			err = errors.New("cached")
+			return body, err
+		}
+	}
+	logger.Debug("Alert Updates")
+	if t != "head" {
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+		l, _ := time.Parse("Mon, 2 Jan 2006 15:04:05 GMT", resp.Header["Last-Modified"][0])
+		LastModified = l
+	}
+
+	return
+}
--- weather-server/go.mod
+++ weather-server/go.mod
@@ -5,13 +5,20 @@ go 1.12
 require (
 	cloud.google.com/go v0.37.4 // indirect
 	github.com/IvanMenshykov/MoonPhase v0.0.0-20180124195458-2a9432a62575
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
+	github.com/dghubble/oauth1 v0.6.0
+	github.com/go-co-op/gocron v0.3.0
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/websocket v1.4.2
-	github.com/jinzhu/gorm v1.9.12
+	github.com/jasonlvhit/gocron v0.0.0-20200423141508-ab84337f7963
+	github.com/jinzhu/gorm v1.9.14
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	github.com/paulmach/go.geojson v1.4.0
 	github.com/sirupsen/logrus v1.6.0
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
