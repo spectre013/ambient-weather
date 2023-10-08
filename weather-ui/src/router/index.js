987162f4ad26feb378be27d128e23376e890764c
Brian Paulson <brian@brianpaulson.com>
2023-10-08 17:25:13 -0600

    Forecast remvoel

--- weather-ui/src/router/index.js
+++ weather-ui/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../components/HomeView.vue'
-import ForecastVue from "../components/ForecastVue.vue";
+//import ForecastVue from "../components/ForecastVue.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/forecast',
-      name: 'forecast',
-      component: ForecastVue
-    }
+    // {
+    //   path: '/forecast',
+    //   name: 'forecast',
+    //   component: ForecastVue
+    // }
   ]
 })
 
